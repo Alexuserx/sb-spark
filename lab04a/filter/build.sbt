1460fe8125b6dc1be5e2fa0f3a4e85f60d2da7ec
Yurchenko Alexander <yurchenko.aa@phystech.edu>
2023-10-29 22:48:34 +0300

    added lab04a raw version

---
+++ lab04a/filter/build.sbt
@@ -0,0 +1,12 @@
+ThisBuild / version := "1.0"
+
+ThisBuild / scalaVersion := "2.11.12"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "filter"
+  )
+
+val sparkVersion = "2.4.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
---
+++ lab04a/filter/src/main/scala/filter.scala
@@ -0,0 +1,86 @@
+import org.apache.spark.sql.{DataFrame, SparkSession, Column}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+import java.net.{URL, URLDecoder}
+import scala.util.Try
+import sys.process._
+
+object filter extends java.io.Serializable {
+  protected def writeDataToHDFS(df: DataFrame, outputDirPrefix: String, path: String): Unit = {
+    val pathToData = outputDirPrefix.stripSuffix("/") + "/" + path.stripPrefix("/")
+    println(s"pathToData: $pathToData")
+    val finalDF = df
+      .toJSON
+      .select(
+        get_json_object(col("value"), "$.value").as("value"),
+        get_json_object(col("value"), "$.p_date").as("p_date")
+      )
+
+    finalDF
+      .write
+      .format("json")
+      .mode("overwrite")
+      .partitionBy("p_date")
+      .save(pathToData)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder()
+      .getOrCreate()
+
+    spark.conf.set("spark.sql.session.timeZone", "UTC")
+    //         val topic: String = spark.sparkContext.getConf.get("spark.filter.topic_name")
+    //         val offset: String = spark.sparkContext.getConf.get("spark.filter.offset")
+    //         val outputDirPrefix: String = spark.sparkContext.getConf.get("spark.filter.output_dir_prefix")
+
+    val topic: String = "lab04_input_data"
+    val offset: String = "earliest"
+    val outputDirPrefix: String = "/user/aleksandr.yurchenko/visits"
+
+    val buyPath: String = "buy"
+    val viewPath: String = "view"
+
+    val valueSchema: StructType = StructType(
+      Seq(
+        StructField("event_type", StringType, nullable=false),
+        StructField("category", StringType, nullable=false),
+        StructField("item_id", StringType, nullable=false),
+        StructField("item_price", StringType, nullable=false),
+        StructField("uid", StringType, nullable=false),
+        StructField("timestamp", StringType, nullable=false)
+      )
+    )
+
+    val df: DataFrame = spark.read.format("kafka")
+      .option("kafka.bootstrap.servers", "spark-master-1:6667")
+      .option("subscribe", topic)
+      .option(
+        "startingOffsets",
+        if (offset.contains("earliest")) offset
+        else "{\"" + topic + "\":{\"0\":" + offset + "}}"
+      )
+      .load()
+    println(s"Loaded data from kafka topic=$topic with offset=$offset")
+
+    val filledWithDateDF: DataFrame = df
+      .withColumn("value", from_json(col("value").cast(StringType), valueSchema))
+      .withColumn("p_date",
+        from_unixtime(col("value.timestamp").cast(LongType) / lit(1000L), "YYYYMMdd").cast(StringType).alias("date"))
+      .select(
+        struct(col("value.*"), col("p_date").alias("date")).alias("value"),
+        col("p_date")
+      )
+      .orderBy(col("p_date").desc)
+
+    val buyTypeDF: DataFrame = filledWithDateDF.filter(col("value.event_type") === lit("buy"))
+    val viewTypeDF: DataFrame = filledWithDateDF.filter(col("value.event_type") === lit("view"))
+
+    println(s"Started saving data to HDFS, outputDirPrefix=$outputDirPrefix")
+    writeDataToHDFS(buyTypeDF, outputDirPrefix, buyPath)
+    println(s"Saved buyTypeDF data to HDFS buyPath=$buyPath")
+    writeDataToHDFS(viewTypeDF, outputDirPrefix, viewPath)
+    println(s"Saved viewTypeDF data to HDFS viewPath=$viewPath")
+  }
+}
